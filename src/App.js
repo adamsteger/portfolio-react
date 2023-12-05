6c596361060c1434f7a14053708a756b10d593f6
Adam Steger <asteger@email.sc.edu>
2023-12-05 17:35:02 -0500

    Added experiences

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import Navbar from "./components/navbar/navbar";
 import Home from "./components/home/home";
 import About from "./components/about/about";
 import Skills from "./components/skills/skills";
+import Experiences from "./components/experiences/experiences";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
       <Home />
       <About />
       <hr />
       <Skills />
+      <hr />
+      <Experiences />
     </div>
   );
 }
---
+++ src/components/experiences/experiences.js
@@ -0,0 +1,109 @@
+import React from "react";
+import "./experiences.css";
+import Swampfox from "../../assets/swampfox-logo.jpg";
+import USC from "../../assets/usc_logo.png";
+import Duck from "../../assets/duckcreek-logo.jpg";
+import UofSC from "../../assets/engineering.png";
+import Gravatt from "../../assets/gravatt.jpg";
+
+const Experiences = () => {
+  return (
+    <section id="experiences">
+      <span className="title">Education & Experiences</span>
+      <p className="description">
+        A list of professional experiences and my educational journey that has
+        led me to where I am today.
+      </p>
+      <div className="card">
+        <img
+          src={USC}
+          alt="University of South Carolina Logo"
+          className="card-img"
+        />
+        <div className="card-text">
+          <div className="card-title">
+            <h2 className="card-title">South Carolina Honors College</h2>
+            <span className="date">Aug 2020 - Dec 2023</span>
+          </div>
+          <h3 className="position-title">
+            B.S. in Computer Science (GPA: 4.0)
+          </h3>
+          <p className="card-description">
+            Excelled in coursework in Data Structures & Algorithms, Software
+            Engineering, Algorithm Design I/II, Computer Networking, and
+            Database System Design.
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <img src={Swampfox} alt="Swampfox Logo" className="card-img" />
+        <div className="card-text">
+          <div className="card-title">
+            <h2 className="card-title">Swampfox Technologies</h2>
+            <span className="date">Sept 2023 - Present</span>
+          </div>
+          <h3 className="position-title">Software Developer Intern</h3>
+          <p className="card-description">
+            Develop GUI with test-driven development using Angular, Javascript,
+            HTML5, and SCSS. Attach GUI to RESTful API and create reusable form
+            components to be used across SF products.
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <img
+          src={Duck}
+          alt="Duck Creek Technologies Logo"
+          className="card-img"
+        />
+        <div className="card-text">
+          <div className="card-title">
+            <h2 className="card-title">Duck Creek Technologies</h2>
+            <span className="date">May 2023 - Aug 2023</span>
+          </div>
+          <h3 className="position-title">Software Configuration Intern</h3>
+          <p className="card-description">
+            Participated in sprints to configure complex SaaS product to
+            business requirements utilizing XML, XPath, Git, and Azure.
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <img src={UofSC} alt="" className="card-img" />
+        <div className="card-text">
+          <div className="card-title">
+            <h2 className="card-title">
+              USC College of Engineering and Computing
+            </h2>
+            <span className="date">Aug 2022 - May 2023</span>
+          </div>
+          <h3 className="position-title">
+            Undergraduate Software Engineering Teaching Assistant
+          </h3>
+          <p className="card-description">
+            Served as SCRUM Master for 7 teams of students developing a Java
+            application. Lead one-on-one meetings with students to teach design
+            patterns and Java fundamentals.
+          </p>
+        </div>
+      </div>
+      <div className="card">
+        <img src={Gravatt} alt="Gravatt Logo" className="card-img gravatt" />
+        <div className="card-text">
+          <div className="card-title">
+            <h2 className="card-title">Gravatt Camp and Conference Center</h2>
+            <span className="date">May 2019 - Aug 2023</span>
+          </div>
+          <h3 className="position-title">Hospitality Director</h3>
+          <p className="card-description">
+            Managed ordering, planning, and organization of all meals for over
+            250 campers and staff daily. Directed managed 6 kitchen assistants
+            to delegate tasks and coordinate schedules.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Experiences;
